2716ad207a945002ea1a82b4e2eed50e9bd30f4c
Marcin Kwiatkowski <markwiatkowski@yahoo.it>
2012-03-19 16:16:29 +0100

    esercitazione

---
+++ esercitazione.js
@@ -0,0 +1,24 @@
+/**
+* apply
+* apply the given function to the given value
+*
+* @param {Function} [if = arguments[0]] function
+* @param {Mixed} [x = arguments[1]] value
+* @return 
+**/
+
+var apply = function (argumentArray) ...
+
+
+/**
+* aa
+* apply the given function to each element of the given array
+*
+* @param {Function} f funtion
+* @param {Function}
+* @param {Mixed}
+**/
+// aa(function (x) {return x*2;})([1,3,5,7,9]); //[2,6,10,14,18]
+var aa = function (x){
+	
+}
