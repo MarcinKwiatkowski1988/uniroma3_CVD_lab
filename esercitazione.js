0917687b6b1f504e9145818db1f5ceea8b08a6b1
Marcin Kwiatkowski <markwiatkowski@yahoo.it>
2012-03-19 16:37:40 +0100

    esercitazione

--- esercitazione.js
+++ esercitazione.js
@@ -1,24 +1,75 @@
-/**
-* apply
-* apply the given function to the given value
-*
-* @param {Function} [if = arguments[0]] function
-* @param {Mixed} [x = arguments[1]] value
-* @return 
-**/
-
-var apply = function (argumentArray) ...
-
-
-/**
-* aa
-* apply the given function to each element of the given array
-*
-* @param {Function} f funtion
-* @param {Function}
-* @param {Mixed}
-**/
-// aa(function (x) {return x*2;})([1,3,5,7,9]); //[2,6,10,14,18]
-var aa = function (x){
-	
+var apply = function(array){
+  var fun = array[0];
+  var arg = array[1];
+  return fun(arg);
 }
+
+
+// aa(function (x) {return x*2;})([1,3,5,7,9]); //[2,6,10,14,18]
+var aa = function(fun){
+  return function(array){
+    var ret;
+    ret = array.map(fun);
+    return ret;
+  }
+}
+
+
+// comp2 ([f, g])(x) --> f(g(x))
+var comp2 = function (arrArgs){
+  var f = arrArgs[0];
+  var g = arrArgs[1];
+  return function(x){
+    return f(g(x));
+  }	
+} 
+
+
+// compN 
+var comp2 = function (arrArgs){
+  var finalFunction = arrArgs.reduce (function (prev, cur)) {
+    return prev(cur);
+  };
+  return function(x){
+    return finalFunction(x);     
+  }	
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
